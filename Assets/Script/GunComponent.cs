9334f6d2473f330f4540fbf90ac675d20df86e2c
KishaanT <143449806+KishaanT@users.noreply.github.com>
2025-09-10 11:55:49 -0400

    I implemented gun mechanics

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -1,13 +1,49 @@
+using System;
 using UnityEngine;
 
 public class GunComponent : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
-    public float bulletMaxImpulse = 10.0f;
-    public float maxChargeTime = 3.0f;
+    public float bulletMaxImpulse = 100.0f;
+    public float maxChargeTime = 5.0f;
     private float chargeTime = 0.0f;
     private bool isCharging = false;
+
+    public void Start()
+    {
+        chargeTime = Mathf.Clamp(chargeTime, 0.0f, maxChargeTime);
+    }
+    public void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            
+            ShootBullet();
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0.0f;
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            chargeTime += Time.deltaTime;
+        }
+    }
+
+    void ShootBullet()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position,
+        bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        //Change that equation so that it adds an impulse that follwos charge time
+        float bulletImpulse = chargeTime/maxChargeTime *  bulletMaxImpulse;
+
+        //An Impulse is a force you apply on a object in a single instant
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
 
    
 }
